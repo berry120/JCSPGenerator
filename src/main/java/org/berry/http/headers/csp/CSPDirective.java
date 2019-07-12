f7a5a2c98dbebafdccd74e28b8c6b84e3dd69159
berry120 <berry120@gmail.com>
2019-07-12 23:42:24 +0100

    Initial commit

---
+++ src/main/java/org/berry/http/headers/csp/CSPDirective.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.berry.http.headers.csp;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ *
+ * @author Michael
+ */
+public class CSPDirective {
+    
+    private String name;
+    private List<String> values;
+    
+    public String getValue() {
+        return name + " " + String.join(" ", values);
+    }
+    
+    public CSPDirective(String name) {
+        this(name, new String[0]);
+    }
+    
+    public CSPDirective(String name, Iterable<String> values) {
+        this(name, StreamSupport.stream(values.spliterator(), false));
+    }
+    
+    public CSPDirective(String name, Stream<String> values) {
+        this(name, values.toArray(String[]::new));
+    }
+    
+    public CSPDirective(String name, String... values) {
+        this.name = name;
+        this.values = Collections.unmodifiableList(Arrays.asList(values));
+    }
+    
+    public static CSPDirective baseUri(String... values) {
+        return new CSPDirective("base-uri", values);
+    }
+    
+    public static CSPDirective blockAllMixedContent() {
+        return new CSPDirective("block-all-mixed-content");
+    }
+    
+    public static CSPDirective childSrc(String... values) {
+        return new CSPDirective("child-src", values);
+    }
+    
+    public static CSPDirective connectSrc(String... values) {
+        return new CSPDirective("connect-src", values);
+    }
+    
+    public static CSPDirective defaultSrc(String... values) {
+        return new CSPDirective("default-src", values);
+    }
+    
+    public static CSPDirective fontSrc(String... values) {
+        return new CSPDirective("font-src", values);
+    }
+    
+    public static CSPDirective formAction(String... values) {
+        return new CSPDirective("form-action", values);
+    }
+    
+    public static CSPDirective frameAncestors(String... values) {
+        return new CSPDirective("frame-ancestors", values);
+    }
+    
+    public static CSPDirective frameSrc(String... values) {
+        return new CSPDirective("frame-src", values);
+    }
+    
+    public static CSPDirective imgSrc(String... values) {
+        return new CSPDirective("img-src", values);
+    }
+    
+    public static CSPDirective manifestSrc(String... values) {
+        return new CSPDirective("manifest-src", values);
+    }
+    
+    public static CSPDirective mediaSrc(String... values) {
+        return new CSPDirective("media-src", values);
+    }
+    
+    public static CSPDirective objectSrc(String... values) {
+        return new CSPDirective("object-src", values);
+    }
+    
+    public static CSPDirective pluginTypes(String... values) {
+        return new CSPDirective("plugin-types", values);
+    }
+    
+    public static CSPDirective referrer(String referrerPolicy) {
+        return new CSPDirective("referrer", referrerPolicy);
+    }
+    
+    public static CSPDirective reportTo(String... values) {
+        return new CSPDirective("report-to", values);
+    }
+    
+    public static CSPDirective reportUri(String... values) {
+        return new CSPDirective("report-uri", values);
+    }
+    
+    public static CSPDirective requireSriFor(String... values) {
+        return new CSPDirective("require-sri-for", values);
+    }
+    
+    public static CSPDirective sandbox(String... values) {
+        return new CSPDirective("sandbox", values);
+    }
+    
+    public static CSPDirective scriptSrc(String... values) {
+        return new CSPDirective("script-src", values);
+    }
+    
+    public static CSPDirective styleSrc(String... values) {
+        return new CSPDirective("style-src", values);
+    }
+    
+    public static CSPDirective upgradeInsecureRequests() {
+        return new CSPDirective("upgrade-insecure-requests");
+    }
+    
+    public static CSPDirective workerSrc(String... values) {
+        return new CSPDirective("worker-src", values);
+    }
+    
+}
---
+++ src/main/java/org/berry/http/headers/csp/CSPHeader.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.berry.http.headers.csp;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ *
+ * @author Michael
+ */
+public class CSPHeader {
+
+    private final List<CSPDirective> directives;
+
+    public CSPHeader(Iterable<CSPDirective> values) {
+        this(StreamSupport.stream(values.spliterator(), false));
+    }
+
+    public CSPHeader(Stream<CSPDirective> values) {
+        this(values.toArray(CSPDirective[]::new));
+    }
+
+    public CSPHeader(CSPDirective... values) {
+        this.directives = Collections.unmodifiableList(Arrays.asList(values));
+    }
+
+    public String getValue() {
+        return String.join("; ",
+                directives.stream()
+                        .map(d -> d.getValue())
+                        .collect(Collectors.toList()));
+    }
+
+    public static void main(String[] args) {
+        CSPHeader header = new CSPHeader(
+                CSPDirective.defaultSrc(CSPConstants.SELF),
+                CSPDirective.connectSrc(CSPConstants.SELF, "https://legacy.example.com"),
+                CSPDirective.fontSrc(CSPConstants.SELF, "https://fonts.example.com"),
+                CSPDirective.frameSrc(CSPConstants.SELF, "https://frame.paypal.com", "https://frame2.paypal.com", "https://analytics.provider.info"),
+                CSPDirective.imgSrc(CSPConstants.SELF, "https://images.example.com", "https://cdn.example.com"),
+                CSPDirective.mediaSrc(CSPConstants.NONE),
+                CSPDirective.objectSrc(CSPConstants.NONE),
+                CSPDirective.scriptSrc(CSPConstants.SELF, "https://script.example.org", "https://apis.example.org", "https://analytics.provider.info"),
+                CSPDirective.frameAncestors(CSPConstants.NONE),
+                CSPDirective.blockAllMixedContent(),
+                CSPDirective.upgradeInsecureRequests(),
+                CSPDirective.upgradeInsecureRequests(),
+                CSPDirective.upgradeInsecureRequests()
+        );
+        System.out.println(header.getValue());
+    }
+
+}
