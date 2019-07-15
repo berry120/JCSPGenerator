3c86e0d867868216616c468dc72e288dafc52229
berry120 <berry120@gmail.com>
2019-07-15 18:20:01 +0100

    Refactoring

--- src/main/java/org/berry/http/headers/csp/CSPUtils.java
+++ src/main/java/org/berry/http/headers/csp/CSPUtils.java
@@ -16,35 +16,34 @@ import java.util.Base64;
  * @author Michael
  */
 public class CSPUtils {
-    
+
     private static final SecureRandom SECURE_RANDOM = new SecureRandom();
-    
+
     public static String generateNonce() {
         byte[] bytes = new byte[16];
         SECURE_RANDOM.nextBytes(bytes);
         return new String(Base64.getEncoder().encode(bytes));
     }
-    
+
     public static String hashSha256(String toHash) {
-        return encode("SHA-256", toHash);
+        return hash(toHash, "SHA-256");
     }
-    
+
     public static String hashSha384(String toHash) {
-        return encode("SHA-384", toHash);
+        return hash(toHash, "SHA-384");
     }
-    
+
     public static String hashSha512(String toHash) {
-        return encode("SHA-512", toHash);
+        return hash(toHash, "SHA-512");
     }
-    
-    private static String encode(String algorithm, String toHash) {
+
+    public static String hash(String toHash, String algorithm) {
         try {
-        return new String(
-                Base64.getEncoder().encode(MessageDigest.getInstance(algorithm).digest(toHash.getBytes(StandardCharsets.UTF_8))));
-        }
-        catch(NoSuchAlgorithmException ex) {
-            throw new AssertionError("Error locating " + algorithm + " algorithm", ex);
+            return new String(
+                    Base64.getEncoder().encode(MessageDigest.getInstance(algorithm).digest(toHash.getBytes(StandardCharsets.UTF_8))));
+        } catch (NoSuchAlgorithmException ex) {
+            throw new IllegalArgumentException("Error locating " + algorithm + " algorithm", ex);
         }
     }
-    
+
 }
