ea7b5c091759046bdf75c8b207d2dcb58b2c5293
berry120 <berry120@gmail.com>
2019-07-15 19:18:42 +0100

    Remove unused import

--- src/main/java/org/berry/http/headers/csp/CSPDirective.java
+++ src/main/java/org/berry/http/headers/csp/CSPDirective.java
@@ -8,8 +8,6 @@ package org.berry.http.headers.csp;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
