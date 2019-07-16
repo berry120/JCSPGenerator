a81181faae6e18f33f5519490d64f013f2da1f7f
berry120 <berry120@gmail.com>
2019-07-16 08:42:28 +0100

    Fix test

--- src/test/java/org/berry/http/headers/csp/CSPSyntaxCheckerTest.java
+++ src/test/java/org/berry/http/headers/csp/CSPSyntaxCheckerTest.java
@@ -21,76 +21,71 @@ public class CSPSyntaxCheckerTest {
     @Test
     public void testCheckNameAndValues() {
         System.out.println("checkNameAndValues");
-        CSPSyntaxChecker instance = new CSPSyntaxChecker();
         assertThrows(InvalidDirectiveValueException.class, () -> {
-            instance.checkNameAndValues("testnameok", new String[]{"valueok", "value notok (contains space)"});
+            CSPSyntaxChecker.checkNameAndValues("testnameok", new String[]{"valueok", "value notok (contains space)"});
         });
         assertThrows(InvalidDirectiveNameException.class, () -> {
-            instance.checkNameAndValues("testname;notok", new String[]{"valueok", "valueok2"});
+            CSPSyntaxChecker.checkNameAndValues("testname;notok", new String[]{"valueok", "valueok2"});
         });
     }
 
     @Test
     public void testCheckValues() {
         System.out.println("checkValues");
-        CSPSyntaxChecker instance = new CSPSyntaxChecker();
-        instance.checkValues(new String[] {"valueok"});
-        instance.checkValues(new String[] {"valueok", "anothervalueok"});
-        instance.checkValues(new String[] {"valueok", "specialsymbolok123!\"" });
+        CSPSyntaxChecker.checkValues(new String[] {"valueok"});
+        CSPSyntaxChecker.checkValues(new String[] {"valueok", "anothervalueok"});
+        CSPSyntaxChecker.checkValues(new String[] {"valueok", "specialsymbolok123!\"" });
         assertThrows(InvalidDirectiveValueException.class, () -> {
-            instance.checkValues(new String[]{"valueok", "value notok (contains space)"});
+            CSPSyntaxChecker.checkValues(new String[]{"valueok", "value notok (contains space)"});
         });
         assertThrows(InvalidDirectiveValueException.class, () -> {
-            instance.checkValues(new String[]{"valu;eok", "value notok (contains space)"});
+            CSPSyntaxChecker.checkValues(new String[]{"valu;eok", "value notok (contains space)"});
         });
         assertThrows(InvalidDirectiveValueException.class, () -> {
-            instance.checkValues(new String[]{"value;notok"});
+            CSPSyntaxChecker.checkValues(new String[]{"value;notok"});
         });
     }
 
     @Test
     public void testCheckName() {
         System.out.println("checkName");
-        CSPSyntaxChecker instance = new CSPSyntaxChecker();
-        instance.checkName("normalname");
+        CSPSyntaxChecker.checkName("normalname");
         assertThrows(InvalidDirectiveNameException.class, () -> {
-            instance.checkName("asd;asd");
+            CSPSyntaxChecker.checkName("asd;asd");
         });
         assertThrows(InvalidDirectiveNameException.class, () -> {
-            instance.checkName("asd asd");
+            CSPSyntaxChecker.checkName("asd asd");
         });
     }
 
     @Test
     public void testCheckDuplicateDirectives() {
         System.out.println("checkDuplicateDirectives");
-        CSPSyntaxChecker instance = new CSPSyntaxChecker();
-        instance.checkDuplicateDirectives(new CSPDirective("onename"), new CSPDirective("anothername"));
+        CSPSyntaxChecker.checkDuplicateDirectives(new CSPDirective("onename"), new CSPDirective("anothername"));
         assertThrows(DuplicateDirectivesException.class, () -> {
-            instance.checkDuplicateDirectives(new CSPDirective("onename"), new CSPDirective("anothername"), new CSPDirective("anothername"));
+            CSPSyntaxChecker.checkDuplicateDirectives(new CSPDirective("onename"), new CSPDirective("anothername"), new CSPDirective("anothername"));
         });
     }
 
     @Test
     public void testCheckBase64() {
         System.out.println("checkBase64");
-        CSPSyntaxChecker instance = new CSPSyntaxChecker();
-        instance.checkBase64("MTIz", 3);
-        instance.checkBase64("MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5", 36);
-        instance.checkBase64("MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5");
-        instance.checkBase64("", 0);
-        instance.checkBase64("YQ==", 1);
+        CSPSyntaxChecker.checkBase64("MTIz", 3);
+        CSPSyntaxChecker.checkBase64("MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5", 36);
+        CSPSyntaxChecker.checkBase64("MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5");
+        CSPSyntaxChecker.checkBase64("", 0);
+        CSPSyntaxChecker.checkBase64("YQ==", 1);
         assertThrows(NotBase64Exception.class, () -> {
-            instance.checkBase64("MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5", 1);
+            CSPSyntaxChecker.checkBase64("MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5MTIzNDU2Nzg5", 1);
         });
         assertThrows(NotBase64Exception.class, () -> {
-            instance.checkBase64("$$", -1);
+            CSPSyntaxChecker.checkBase64("$$", -1);
         });
         assertThrows(NotBase64Exception.class, () -> {
-            instance.checkBase64("notbase64atall!!!!");
+            CSPSyntaxChecker.checkBase64("notbase64atall!!!!");
         });
         assertThrows(NotBase64Exception.class, () -> {
-            instance.checkBase64("MTIz", 0);
+            CSPSyntaxChecker.checkBase64("MTIz", 0);
         });
     }
 
