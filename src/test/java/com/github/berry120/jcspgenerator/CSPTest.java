734deea12277e5f8bce6ae471a97cf93d07b46fa
Michael <michael@asperato.com>
2019-08-07 14:48:27 +0100

    Fix test package name

--- src/test/java/com/github/berry120/jcspgenerator/CSPTest.java
+++ src/test/java/com/github/berry120/jcspgenerator/CSPTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.berry.http.headers.csp;
+package com.github.berry120.jcspgenerator;
 
 import com.github.berry120.jcspgenerator.CSP;
 import com.github.berry120.jcspgenerator.CSPDirective;
